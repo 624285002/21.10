670ba8e6f2009b1897961ca61a19501aaeb5412c
624285002 <combo7238@gmail.com>
2022-10-28 11:11:55 +0700

    .commit

--- Home.py
+++ Home.py
@@ -29,8 +29,15 @@ dt3 = dz['sepal.length'].sum()
 dt4 = dz['sepal.width'].sum()
 dx=[dt1,dt2,dt3,dt4]
 dx2=pd.DataFrame(dx,index=["d1","d2","d3","d4"])
-st.bar_chart(dx2)
+
 st.balloons()
+
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+   st.write(dz.head(20))
+   st.bar_chart(dx2)
+   st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
 
 st.sidebar.markdown("# วิเคราห์รายบุคคล")
 
